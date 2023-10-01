8f32d25a10cd7533a03cb4474ffb8dd2b0c7757a
Afzal <afzalashraf814@gmail.com>
2023-10-01 18:07:44 +0500

    🐛 fix: change next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
+  output: 'export',
+  basePath: '/mafzaldev.github.io',
+
   images: {
     remotePatterns: [
       {
